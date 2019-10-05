e0754ad660e61cdc283c24ef10b4c372a721a754
Bryant Nguyen <bryant_72002@yahoo.com>
2019-10-05 12:36:41 -0700

    updated local storage, storage elements

--- assets/javascript/budgetPage.js
+++ assets/javascript/budgetPage.js
@@ -1,17 +1,25 @@
 document.getElementById('budgetButton').addEventListener('click', e => {
   e.preventDefault()
   console.log(document.getElementById('budgetHotel').value)
-  let at_ls = JSON.parse(localStorage.getItem('adventureTime'))
-  localStorage.setItem('adventureTime', JSON.stringify({
-    location: at_ls.location,
-    hotelBudget: document.getElementById('budgetHotel').value,
-    restaurantBudget: document.getElementById('budgetRestaurant').value,
-    activitiesBudget: document.getElementById('budgetActivities').value,
-    nightClubBudget: document.getElementById('budgetNightClub').value,
-    theme: ''
-  }))
-  window.location.href = './tile.html'
-})
+  let hotel = document.getElementById('budgetHotel').value
+  let restaurant = document.getElementById('budgetRestaurant').value
+  let activities = document.getElementById('budgetActivities').value
+  let nightClub = document.getElementById('budgetNightClub').value
+
+  if (hotel && restaurant && activities && nightClub) {
+
+    let at_ls = JSON.parse(localStorage.getItem('adventureTime'))
+    localStorage.setItem('adventureTime', JSON.stringify({
+      location: at_ls.location,
+      hotel: hotel,
+      restaurant: restaurant,
+      activities: activities,
+      nightClub: nightClub,
+      theme: ''
+    }))
+      window.location.href = './tile.html'
+    }
+  })
 
 //only move to next page if all slots are filled out
 
