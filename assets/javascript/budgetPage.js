53c395ce614a686301b1355dbbf6affedaf794db
Bryant Nguyen <bryant_72002@yahoo.com>
2019-10-05 11:50:40 -0700

    fixed budgetpage js

--- assets/javascript/budgetPage.js
+++ assets/javascript/budgetPage.js
@@ -1,11 +1,19 @@
-// document.getElementById('budgetButton').addEventListener('click', e => {
-//   e.preventDefault()
-//   console.log(document.getElementById('newBudget').value)
-//   let at_ls = JSON.parse(localStorage.getItem('adventureTime'))
-//   localStorage.setItem('adventureTime', JSON.stringify({
-//     location: at_ls.location,
-//     budget: document.getElementById('newBudget').value,
-//     theme: ''
-//   }))
-//   window.location.href = './tile.html'
-// })
+document.getElementById('budgetButton').addEventListener('click', e => {
+  e.preventDefault()
+  console.log(document.getElementById('budgetHotel').value)
+  let at_ls = JSON.parse(localStorage.getItem('adventureTime'))
+  localStorage.setItem('adventureTime', JSON.stringify({
+    location: at_ls.location,
+    hotelBudget: document.getElementById('budgetHotel').value,
+    restaurantBudget: document.getElementById('budgetRestaurant').value,
+    activitiesBudget: document.getElementById('budgetActivities').value,
+    nightClubBudget: document.getElementById('budgetNightClub').value,
+    theme: ''
+  }))
+  window.location.href = './tile.html'
+})
+
+//only move to next page if all slots are filled out
+
+
+// console.log(hotelBudget)
