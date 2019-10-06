f0289fce5c24ace02f2b987e46b36b0392d21937
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-10-06 01:48:45 -0700

    Fixed bug on tile.js, organized list.js, added continuous scrolling to list.js

--- assets/javascript/tile.js
+++ assets/javascript/tile.js
@@ -3,10 +3,10 @@ document.querySelector('body').addEventListener('click', e => {
     let at_ls = JSON.parse(localStorage.getItem('adventureTime'))
     localStorage.setItem('adventureTime', JSON.stringify({
       location: at_ls.location,
-      hotelBudget: at_ls.hotelBudget,
-      restaurantBudget: at_ls.restaurantBudget,
-      activitiesBudget: at_ls.activitiesBudget,
-      nightClubBudget: at_ls.nightClubBudget,
+      hotel: at_ls.hotel,
+      restaurant: at_ls.restaurant,
+      activities: at_ls.activities,
+      nightClub: at_ls.nightClub,
       theme: e.target.dataset.theme
     }))
     window.location.href = './list.html'
