c209169715149fd827664e18b9cddf532e7acab9
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-10-08 19:10:43 -0700

    Modified weather page

--- assets/javascript/authentication.js
+++ assets/javascript/authentication.js
@@ -3,14 +3,6 @@ document.querySelector('#user-name').innerHTML = ''
 // Initialize the FirebaseUI Widget using Firebase.
 let ui = new firebaseui.auth.AuthUI(firebase.auth())
 
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    // List of OAuth providers supported.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
-})
-
 const uiConfig = {
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
